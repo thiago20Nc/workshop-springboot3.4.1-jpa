130d05fb839ee0b884bf62ab86d942197e83d3ff
Thiago Novaes <novaesthiago27@gmail.com>
2025-01-03 11:52:52 -0300

    Service layer, component registration

---
+++ src/main/java/com/thiagoNc20/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.thiagoNc20.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thiagoNc20.course.entities.User;
+import com.thiagoNc20.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository; 
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
